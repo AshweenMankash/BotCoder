18516d8483856fdbaf26ed312609b30dfa47cf86
Ashween Mankash <Ashweenmankash@gmail.com>
2019-07-24 18:10:53 +0530

    Added some conditions

--- index.js
+++ index.js
@@ -17,19 +17,23 @@ app.get('/', function(req, res) {
 
 
 const newLocal = app.post('/', function(req, res) {
-    console.log(req.body);
+    console.log(req.body.event.type + "   " + req.body.event.text);
+
     res.sendStatus(200);
-    var data = {
-        form: {
-            token: token,
-            channel: req.body.event.channel,
-            text: "Hi! :wave: \n I'm your Uncle, Uncle bob!."
+    if (req.body.event.type == "message" && req.body.event.text) {
+        if (req.body.event.text.includes("hey")) {
+            var data = {
+                form: {
+                    token: token,
+                    channel: req.body.event.channel,
+                    text: "Hi! :wave: \n I'm your Uncle, Uncle bob!."
+                }
+            };
+
+            request.post('https://slack.com/api/chat.postMessage', data, function(error, response, body) {
+                res.json();
+            });
         }
-    };
-    if (req.body.event.text.includes("hey")) {
-        request.post('https://slack.com/api/chat.postMessage', data, function(error, response, body) {
-            res.json();
-        });
     }
 
     const challenge = req.body.challenge;
@@ -38,7 +42,7 @@ const newLocal = app.post('/', function(req, res) {
 app.listen(8080);
 
 app.post('/hi', function(req, res) {
-    console.log(req.body.channel);
+    console.log(req.body);
     var data = {
         form: {
             token: token,
@@ -51,6 +55,18 @@ app.post('/hi', function(req, res) {
     });
 });
 
+app.post('/question', function(req, res) {
+    var data = {
+        form: {
+            token: token,
+            channel: "#general",
+            text: "Question:\n SEARCH ON GOOGLE LAZY BASTARD."
+        }
+    };
+    request.post('https://slack.com/api/chat.postMessage', data, function(error, response, body) {
+        res.json();
+    });
+});
 app.post('/', function(req, res, next) {
     console.log(req.body);
 });
