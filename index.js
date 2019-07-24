0d0e138b5f6ee0dbc2aec1dc1afde201f08aacf2
Ashween Mankash <Ashweenmankash@gmail.com>
2019-07-24 17:15:28 +0530

    Added hey properties

--- index.js
+++ index.js
@@ -11,11 +11,27 @@ app.use(express.json())
 
 app.get('/', function(req, res) {
     console.log("Sent");
+
     res.send("<h1>Ashween Mankash</h1>");
 });
 
 
 const newLocal = app.post('/', function(req, res) {
+    console.log(req.body);
+    res.sendStatus(200);
+    var data = {
+        form: {
+            token: token,
+            channel: req.body.event.channel,
+            text: "Hi! :wave: \n I'm your Uncle, Uncle bob!."
+        }
+    };
+    if (req.body.event.text.includes("hey")) {
+        request.post('https://slack.com/api/chat.postMessage', data, function(error, response, body) {
+            res.json();
+        });
+    }
+
     const challenge = req.body.challenge;
     res.send(challenge);
 });
@@ -33,4 +49,8 @@ app.post('/hi', function(req, res) {
     request.post('https://slack.com/api/chat.postMessage', data, function(error, response, body) {
         res.json();
     });
+});
+
+app.post('/', function(req, res, next) {
+    console.log(req.body);
 });
