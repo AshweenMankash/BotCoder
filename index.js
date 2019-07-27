1a9a499cae18d5fc9164b0650e2633b56cbd1736
Ashween Mankash <Ashweenmankash@gmail.com>
2019-07-27 23:25:01 +0530

    Did something now forgot

--- index.js
+++ index.js
@@ -16,14 +16,13 @@ app.get('/', function(req, res) {
 });
 
 
-const newLocal = app.post('/', function(req, res) {
-    res.sendStatus(200);
+app.post('/', function(req, res) {
 
     if (req.body.event) {
-
+        console.log(req.body);
 
         var text = requestType.hello(req.body.event);
-
+        console.log(text);
         var data = {
             form: {
                 token: token,
@@ -37,37 +36,10 @@ const newLocal = app.post('/', function(req, res) {
                 res.json();
             });
     }
-    const challenge = req.body.challenge;
-    res.send(challenge);
+    if (req.body.challenge) {
+
+        const challenge = req.body.challenge;
+        res.send(challenge);
+    }
 });
 app.listen(8080);
-
-app.post('/hi', function(req, res) {
-    console.log(req.body);
-    var data = {
-        form: {
-            token: token,
-            channel: "#general",
-            text: "Hi! :wave: \n I'm your Uncle, Uncle bob!."
-        }
-    };
-    request.post('https://slack.com/api/chat.postMessage', data, function(error, response, body) {
-        res.json();
-    });
-});
-
-app.post('/question', function(req, res) {
-    var data = {
-        form: {
-            token: token,
-            channel: "#general",
-            text: "Question:\n SEARCH ON GOOGLE LAZY BASTARD."
-        }
-    };
-    request.post('https://slack.com/api/chat.postMessage', data, function(error, response, body) {
-        res.json();
-    });
-});
-app.post('/', function(req, res, next) {
-    console.log(req.body);
-});
